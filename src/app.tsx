2ad6e47aabb3f20c738b02154c9496ecc889dab8
Abilov599 <abilovv599@gmail.com>
2024-12-10 18:14:20 +0400

    little changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
 import { AppRouter } from '@/router/router';
 import { AntdApp } from '@/components/ui/antd-app';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/query-client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
-    <AntdApp>
-      <AppRouter />
-    </AntdApp>
+    <QueryClientProvider client={queryClient}>
+      <AntdApp>
+        <AppRouter />
+      </AntdApp>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { queryClient } from '@/lib/query-client';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 import { App } from '@/app';
 
 import '@/assets/css/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
